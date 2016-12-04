c48465a963f3883589a2bba24c6de58b75670fcc
Wilson Ng <Wilson@Wilsons-MacBook-Pro.local>
2016-12-03 20:00:40 -0800

    Implement express to serve client files - USE PATH

--- server-config.js
+++ server-config.js
@@ -2,11 +2,27 @@ var express = require('express');
 var Movie = require('./App/Models/movie');
 var mongooseConnection = require('./app/config.js');
 
+var path = require('path');
+
 var app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client/index.html');
-})
+app.use(express.static(__dirname + '/client/'));
+
+// app.use(express.static(path.join(__dirname, 'client')));
+
+// app.use("/styles", express.static(__dirname + '/client'));
+
+// app.use('/static', express.static(__dirname + '/client'));
+
+
+// WORKS
+// app.get('/client/styles/style.css', (req, res) => {
+//   res.sendFile(__dirname + '/client/styles/styles.css')
+// })
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/client/index.html');
+// })
 
 app.get('/addMovie', (req, res) => {
   var movie = new Movie({name: 'Saving Private Katharine'});
@@ -17,5 +33,9 @@ app.get('/addMovie', (req, res) => {
     console.log('MOVIE SAVED', movie);
   })
 })
+
+// app.get('/getMovies ', (req, res) => {
+
+// })
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var app = require('./server-config');
 
-
 var port = process.env.PORT || 3000;
 
 app.listen(port);
+
+console.log(__dirname +'/client');
 
 console.log('Server now listening on port ' + port);
