69b2f397191b4634d5bfb6486aa7bb759e0f7bf4
Wilson Ng <Wilson@Wilsons-MacBook-Pro.local>
2016-12-04 14:44:30 -0800

    Add express get request redirects/status codes

--- server-config.js
+++ server-config.js
@@ -1,14 +1,18 @@
 var express = require('express');
+// var bodyparser = require('body-parser');
+var Promise = require('bluebird');
 var Movie = require('./App/Models/movie');
 var mongooseConnection = require('./app/config.js');
 
 var path = require('path');
 var app = express();
+// var router = express.Router();
 
 // this works as well
 // app.use(express.static(__dirname + '/client/'));
 
 app.use(express.static(path.join(__dirname, 'client')));
+// app.use(router);
 
 app.get('/addMovie', (req, res) => {
   var movie = new Movie({name: 'Saving Private Katharine'});
@@ -17,7 +21,8 @@ app.get('/addMovie', (req, res) => {
       return console.error(err);
     }
     console.log('MOVIE SAVED', movie);
-  })
+    res.status(201).redirect('/');
+  });
 });
 
 app.get('/getMovies', (req, res) => {
@@ -28,8 +33,9 @@ app.get('/getMovies', (req, res) => {
     }
     movies.forEach((movie) => {
       console.log(movie);
-    })
-  })
+    });
+  });
+  res.status(200).redirect('/');
 });
 
 module.exports = app;
